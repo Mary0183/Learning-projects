e3a3fe0770b44d4b1f7ae7cb7f4ac745cc116a74
Mary <marysallent@gmail.com>
2020-06-30 15:47:09 +0200

    CodeOp excersices

---
+++ Flipping a Coin/FlippingCoins/EvenNumber.py
@@ -0,0 +1,15 @@
+
+        
+
+def even_number(a):
+        if (a % 2) == 0:
+                return True
+        else:
+                return False
+
+
+print (even_number(7))
+print (even_number(10))
+
+
+
